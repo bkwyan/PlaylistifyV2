acdae4d547085807b79f4f3eb155e7f0cfc9a5c6
Brandon Yan <byan54@uwo.ca>
2020-11-19 12:16:20 -0500

    Changed gitignore

--- server/server.js
+++ server/server.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const querystring = require('querystring');
+const request = require('request');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 

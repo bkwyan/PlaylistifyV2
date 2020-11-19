fd4a409ba02539f191cd9fd8e62cf0206143ec48
Brandon Yan <byan54@uwo.ca>
2020-11-19 17:33:33 -0500

    Finished backend API calls

--- server/server.js
+++ server/server.js
@@ -19,6 +19,11 @@ const app = express();
 // Key for the authorization state
 const stateKey = 'spotify_auth_state';
 
+// Requires format to be "Basic: *<base64 encoded client_id:clientsecret>*"
+const headers = {
+    'Authorization': 'Basic ' + new Buffer(`${CLIENT_ID}:${CLIENT_SECRET}`.toString('base64'))
+}
+
 // Function to generate a random string for the state
 const generateRandomString = (length) => {
     var text = '';
@@ -61,7 +66,90 @@ app.get('/login', (req, res) => {
 app.get('/callback', (req, res) => {
     // If the user accepts the request, in the callback response query string it will have a code and state parameter
     // If the user doesn't accept, in the callback response query string it will have an error and state parameter
-})
+
+    // Authorization code that is used to exchange for an access token
+    let code = req.query.code || null;
+    let state = req.query.state;
+
+    // Access the stored state that was generated when logging in so we can compare it to the state that was returned
+    let storedState = req.cookies ? req.cookies[stateKey] : null;
+
+    // If the state doesn't match then theres a problem!
+    if(state !== storedState){
+        res.redirect('/#' + querystring.stringify({
+            error: 'state_mismatch'
+        }));
+    } else{
+        res.clearCookie(stateKey);
+        let postParams = {
+            url: 'https://accounts.spotify.com/api/token',
+            form: {
+                grant_type: 'authorization_code',
+                code: code,
+                redirect_uri: REDIRECT_URI
+            },
+            headers: headers,
+            json: true
+        };
+
+        request.post(postParams, (error, response, body) => {
+            // Response will have a status code of 200 if success
+            if(!error && response.statusCode === 200){
+                
+                let access_token = body.access_token,
+                    refresh_token = body.refresh.access_token;
+
+                let spotifyAPIOptions = {
+                    url: 'https://api.spotify.com/v1/me',
+                    headers: {
+                        'Authorization': `Bearer ${access_token}`
+                    },
+                    json: true
+                }
+
+                request.get(spotifyAPIOptions, (error, response, body) => {
+                    console.log(body);
+                })
+
+                // Store token in browser to make requests in the front end
+                res.redirect(`${FRONTEND_URI}/#${querystring.stringify(
+                    access_token,
+                    refresh_token
+                )}`);
+            } else{
+                res.redirect('/#' + 
+                    querystring.stringify({
+                        error: 'code'
+                    })
+                );
+            }
+        });
+    }
+});
+
+// API call to get a new token
+app.get('/refresh_token', (req, res) => {
+    // We stored the refresh token in the query
+    let refresh_token = req.query.refresh_token;
+    let postParams = {
+        url: 'https://accounts.spotify.com/api/token',
+        form:{
+            grant_type: 'refresh_token',
+            refresh_token: refresh_token
+        },
+        headers: headers,
+        json: true
+    }
+
+    request.post(postParams, (error, response, body) => {
+        if(!error && response.statusCode === 200){
+            let access_token = body.access_token;
+            res.send({
+                'access_token': access_token
+            });
+        }
+    });
+});
 
 app.listen(PORT, () => {
     console.log('Server is up and running on port number ' + PORT);
